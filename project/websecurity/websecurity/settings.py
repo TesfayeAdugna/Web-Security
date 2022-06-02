8ca9b2b25394d79455e513a0d46a2408639fd392
TesfayeAdugna <adugnatesfaye888@gmail.com>
2022-06-02 20:28:20 +0300

    some changes

--- project/websecurity/websecurity/settings.py
+++ project/websecurity/websecurity/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'login_app.apps.LoginAppConfig'
+    'login_app.apps.LoginAppConfig',
 ]
 
 MIDDLEWARE = [
